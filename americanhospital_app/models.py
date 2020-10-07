3cfb71d51c5eaf877a0b615e45b418310ed66da2
fuad <quluzadef4@gmail.com>
2020-10-07 23:41:28 +0400

    doctor

--- americanhospital_app/models.py
+++ americanhospital_app/models.py
@@ -101,7 +101,7 @@ class PageText(models.Model):
     desc_gr = models.CharField(max_length=255, null=True, blank=True)
     title_tr = models.CharField(max_length=255, null=True, blank=True)
     desc_tr = models.CharField(max_length=255, null=True, blank=True)
-    
+
     def __str__(self):
         return f'{self.name}'
 
@@ -121,7 +121,7 @@ class News(models.Model):
     description_gr = models.TextField()
     description_ru = models.TextField()
     description_tr = models.TextField()
-    keywords = models.TextField(null=True,blank=True)
+    keywords = models.TextField(null=True, blank=True)
     post_type = models.CharField(max_length=20)
     display_home = models.CharField(max_length=1, default="1")
     status = models.CharField(max_length=1, default="1")
